fb7837478993f773dafe46a743a0c69b9cdc348d
Jahaziel <jahaziel.vaz@gmail.com>
2018-01-24 20:14:42 -0700

    updated file to only reflect command line parameter

--- runner.js
+++ runner.js
@@ -4,5 +4,11 @@ var fs = require('fs');
 
 fs.readFile('./countries.json', 'utf8', function(err, data){
   var ourJason = JSON.parse(data);
-  console.log(ourJason[3].name);
+  var ourCountry;
+  for(var index in ourJason) {
+    if(ourJason[index].name === process.argv[2]) {
+      ourCountry = ourJason[index].name;
+    }
+  };
+  console.log(ourCountry);
 });
